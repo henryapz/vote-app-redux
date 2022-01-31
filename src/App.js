c6dd70a32fdeb7a81ec97728cef18b002fddbe14
Henry Aurelio Aymara Apaza <henrys4hgb@gmail.com>
2022-01-31 17:09:37 -0500

    Adding components to App.js and some styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import Candidates from "./components/Candidates";
+import TotalVotes from "./components/TotalVotes";
+import VotesPerCandidate from "./components/VotesPerCandidate";
+import FilterComponent from "./components/FilterComponent";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="title">Election's Day</h1>
+      <Candidates />
+      <div style={{ display: "flex" }}>
+        <FilterComponent />
+        <div className="vote-counter">
+          <TotalVotes />
+          <VotesPerCandidate />
+        </div>
+      </div>
     </div>
   );
 }
